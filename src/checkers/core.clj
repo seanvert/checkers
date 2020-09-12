03192edf034ed0ca5d68c0fa86cd4fdd8b8a39f7
seanvert <seankuchida@gmail.com>
2020-09-12 03:37:52 -0300

    troquei a notação para o padrão de xadrez

--- src/checkers/core.clj
+++ src/checkers/core.clj
@@ -6,10 +6,105 @@
   [& args]
   (println "Hello, World!"))
 
-(defn list-legal-moves
+(defn mainloop
   []
   "todo")
 
-(defn mainloop
-  []
-  "todo") 
+(def initial-table-state
+  {
+   ;; white pieces
+   :a1 1
+   :b2 1
+   :c1 1
+   :d2 1
+   :e1 1
+   :f2 1
+   :g1 1
+   :h2 1
+   :a3 1
+   :c3 1
+   :e3 1
+   :g3 1
+   ;; empty squares
+   :a5 0
+   :b4 0
+   :c5 0
+   :d4 0
+   :e5 0
+   :f4 0
+   :g5 0
+   :h4 0
+   ;; black pieces
+   :a7 2
+   :b8 2
+   :b6 2
+   :c7 2
+   :d8 2
+   :d6 2
+   :e7 2
+   :f8 2
+   :f6 2
+   :g7 2
+   :h8 2
+   :h6 2
+   })
+
+(defn get-key-from-string
+  [entry]
+  (read-string (str ":" entry)))
+
+;; padrão que eu vou seguir para montar o esquema dos quadrados
+;; adjacentes
+;; <letra><dígito>
+;; casas adjacentes:
+;; <letra + 1><número + 1>
+;; <letra><número +- 1>
+;; <letra - 1><número - 1>
+
+(defn list-adjacent-squares
+  ;; pega uma string no formato "<letra><número>"
+  ;; ex: a1 b4 c2 d7
+  [square]
+  (let [letter (first square)
+        index (subs square 1 2)
+        int-letter (int letter)
+        int-index (Integer/parseInt index)
+        make-key #(get-key-from-string (str %1 %2)) ;; pega a %1 letra e o %2 número
+        ]
+    ;; código ascii da letra em int a == 97
+    (def lower-letter (if (> (dec int-letter) 96)
+                        (char (dec int-letter))
+                        \j))
+    (def upper-letter (char (inc int-letter)))
+    ;; index
+    (def lower-index (dec int-index))
+    (def upper-index (inc int-index))
+    (list
+     (make-key lower-letter lower-index)
+     (make-key lower-letter upper-index)
+     (make-key upper-letter upper-index)
+     (make-key upper-letter lower-index)
+     )))
+
+(list-adjacent-squares "d3")
+
+(defn check-adjacent-squares-for-pieces
+  [square]
+  ;; pegar o estado atual do tabuleiro
+  (map (fn [x] (get initial-table-state x)) (list-adjacent-squares square))
+  )
+
+(check-adjacent-squares-for-pieces "b8")
+(defn list-available-pieces
+  [x]
+  (if (= x "w")
+    "white pieces"
+    "black pieces"))
+
+
+(defn list-legal-moves
+  ;; start with regular stuff
+  ;; then implement capturing
+  [x] ;; gets a square in 'a1' format
+  (let [adjacent (list-adjacent-squares x)]
+    ))
--- test/checkers/core_test.clj
+++ test/checkers/core_test.clj
@@ -7,10 +7,15 @@
 ;;     (is (= 0 1))))
 
 (deftest mov-test
-  (testing "Teste dos movimentos possíveis"
-      (is (= (list-legal-moves) "todo")))
-  )
+  (testing "Teste dos movimentos possíveis stub"
+      (is (= (list-legal-moves) "todo"))))
 
 (deftest state-test
-  (testing "Teste do estado do tabuleiro"
+  (testing "Teste do estado do tabuleiro stub"
     (is (= (mainloop) "todo"))))
+
+(deftest adjacent-squares-test
+  (testing "Teste de casas adjacentes"
+    (is (= (list-adjacent-squares "b3") (hash-set :a2 :b2 :c4 :b4)))
+    (is (= (list-adjacent-squares "c4") (hash-set :b3 :c3 :c5 :d5)))
+    ))
